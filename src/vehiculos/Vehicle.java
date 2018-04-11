2e42d4408a5e417b812da07bd3bf073febfcc5ed
cfgs <cfgs@pc-info-host>
2018-04-11 20:32:30 +0200

    Vehiculo: variables y metodo de acceleracion

--- src/vehiculos/Vehicle.java
+++ src/vehiculos/Vehicle.java
@@ -3,16 +3,15 @@ package vehiculos;
 public class Vehicle {
 	private final int velocitat_maxima = 120;
 	private int cilindrada;
-	private int portes;
+	private int portes ;
 	private int velocitat_actual = 0;
 
 
-	//CONTROL ACCELERACIO-------
+	//CONTROL ACCELERACIO
 
 	public void accelerar(){
 		while(this.velocitat_actual < velocitat_maxima){
 			this.velocitat_actual += 10;
 		}
 	}
-
 }
